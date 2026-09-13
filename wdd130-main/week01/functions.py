8ed35660f2e197089493d7eb37b4672de20657b0
your name <your.mail@example.com>
2026-09-13 22:39:15 +0300

    fixed image

---
+++ wdd130-main/week01/functions.py
@@ -0,0 +1,41 @@
+
+
+
+def add(a, b):
+    result = a + b
+    return result
+
+
+###### why main Exists #####
+total = add(3, 7)
+# total = 10
+print(total)
+
+
+#########################
+#### why Main Exists ####
+#########################
+
+
+def main():
+    # x = 3
+    # y = 7
+    # total = add(x, y)
+    # print(total)
+
+
+    #def add (a, b)
+    #    result = a + b
+    total = add(3, 7)
+
+#main()
+##########################################
+#### local variables exist and scores ####
+##########################################
+
+
+    print("The sum is:", total)
+
+
+if __name__ == "__main__":
+    main()
